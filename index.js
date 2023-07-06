fc233fd7d0ab432f00719f999b1b5a0385709cf5
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-06 15:56:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+ function isPalindrome(word) {
+    // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  //convert word string to lowercas and remove spaces and store in words variable
+//reverse the string
+//check if the original string and reversed string are equal
+//return true if they are equalor false otherwise
 */
 
 /*
   Add written explanation of your solution here
+    //convert the string to lowercase and remove spaces
+    word = lowercase(str)
+    word = removeSpaces(str)
+  //reverse the string
+    reversedStr = reverseString(word)
+   //check if the original string and reversed string are equal
+    if word equals reversedword:
+        return true
+    else:
+        return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +42,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+ 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 }
 
 module.exports = isPalindrome;
